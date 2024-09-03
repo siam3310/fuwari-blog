1d9aa926bac813315dd2ff87dba12abb9a3a6127
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-09-03 08:31:58 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://siamsblog2.netlify.app/",
   base: "/",
   trailingSlash: "always",
   integrations: [
